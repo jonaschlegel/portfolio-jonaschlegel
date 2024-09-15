a666c96d311e23871a031733183cc294af4ba14c
jonaschlegel <jona.schlegel@gmail.com>
2024-09-15 16:03:05 +0200

    Add external website links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { FiExternalLink } from 'react-icons/fi';
 import { MdDehaze } from 'react-icons/md';
 
 import { navLinks } from '@/data/content';
@@ -8,38 +9,69 @@ import Logo from '@/logo/Logo';
 import Button from '@/shared/Buttons/Button';
 import ButtonSecondary from '@/shared/Buttons/ButtonSecondary';
 
+const externalLinks = [
+  {
+    name: 'The TrowelJournal Blog',
+    shortName: 'blog',
+    href: 'https://www.troweljournal.com/',
+  },
+  {
+    name: 'The archaeoINK newsletter',
+    shortName: 'newsletter',
+    href: 'https://archaeoink.ck.page/',
+  },
+];
+
 const Navbar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleToggleMenu = () => {
     setMobileOpen((prev) => !prev);
   };
+
   return (
     <div className="container mx-auto mb-0 flex items-center justify-between pb-2 pt-4">
       <div className="z-30">
         <Logo />
       </div>
       <div className="z-30 lg:hidden">
-        <button type="button" aria-label="Save" onClick={handleToggleMenu}>
+        <button
+          type="button"
+          aria-label="Toggle menu"
+          onClick={handleToggleMenu}
+        >
           <MdDehaze />
         </button>
       </div>
       <div
-        className={`fixed top-0 z-10 flex  h-screen w-screen items-center bg-neutral-950 transition-all duration-500 lg:bg-transparent ${
+        className={`fixed top-0 z-10 flex h-screen w-screen items-center bg-neutral-950 transition-all duration-500 ${
           mobileOpen ? 'left-0' : '-left-full'
-        }
-            lg:static lg:size-auto`}
+        } lg:static lg:size-auto lg:bg-transparent`}
       >
         <ul className="flex w-full flex-col items-center gap-6 lg:flex-row lg:gap-8">
-          {navLinks.map((navItem) => {
-            return (
-              <li key={navItem.name}>
-                <Button href={navItem.href} onClick={handleToggleMenu}>
-                  {navItem.name}
-                </Button>
-              </li>
-            );
-          })}
+          {navLinks.map((navItem) => (
+            <li key={navItem.name}>
+              <Button href={navItem.href} onClick={handleToggleMenu}>
+                {navItem.name}
+              </Button>
+            </li>
+          ))}
+          {externalLinks.map((link) => (
+            <li key={link.name}>
+              <a
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={handleToggleMenu}
+                className="flex items-center text-primary-accent"
+              >
+                <span className="lg:hidden">{link.name}</span>
+                <span className="hidden lg:inline">{link.shortName}</span>
+                <FiExternalLink className="ml-1 size-4" />
+              </a>
+            </li>
+          ))}
+
           <li className="lg:hidden">
             <ButtonSecondary onClick={handleToggleMenu}>
               Contact Me
